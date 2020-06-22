7381c6aa42269937b8765cb47203ad5c68dfb747
syang-lab <62822921+syang-lab@users.noreply.github.com>
2020-06-21 20:23:10 -0400

    commit-2020-version1

---
+++ embeded_vgg.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 00:05:30 2020
+
+@author: shanyang
+"""
+## for building DNN model 
+from keras import Sequential 
+from keras.layers import Dense
+from keras import initializers 
+from keras import optimizers
+## train test split 
+from sklearn.model_selection import train_test_split
+
+
+## for data preprocessing 
+import os 
+import glob
+import numpy as np
+from PIL import Image
+
+## for FeatureEmbemding 
+from keras_vggface.vggface import VGGFace
+from sklearn.preprocessing import MinMaxScaler
+
+
+class PreprocessData:
+    def load_label_data(self,input_dir):
+        X = list();
+        Y = list();
+
+        for image_dir in os.listdir(input_dir):
+            if (image_dir=='true'):
+                label = 1
+            else:
+                label = 0
+            
+            image_paths = glob.glob(os.path.join(input_dir,image_dir,'*.jpg'))            
+            
+            for image_file in image_paths:
+                image = Image.open(image_file)
+                image = image.convert('RGB')
+                image = np.asarray(image)
+                X.append(image/255)
+                Y.append(label)
+                
+        self.X = np.asarray(X);
+        self.Y = np.asarray(Y);
+        return self.X , self.Y
+
+    def save(self,path):
+        np.savez_compressed(path, X=self.X, Y=self.Y)
+        
+class FeatureEmbemding:
+    """Use VGGFace model to do feature embemding""" 
+    def __init__(self,X,Y):
+        self.X = X;
+        self.label = Y;
+    
+    def generate_features(self):
+        base_model = VGGFace(model='vgg16')
+        self.features_raw = base_model.predict(self.X);
+        
+    def feature_rescale(self):
+        scaler = MinMaxScaler();
+        self.feature = scaler.fit_transform(self.features_raw);
+        
+    def get_feature_label(self):
+        return self.feature, self.label
+    
+    def save(self,path):
+        np.savez_compressed(path, feature=self.feature, label=self.label)
+    
+
+class MyModel:
+    def build_model(self,lr=0.001,decay = 0.01):
+        self.model = Sequential();
+        init = initializers.RandomNormal(mean=0, stddev=1, seed=None);
+        self.model.add(Dense(1024, input_shape=(2622,), activation='relu',kernel_initializer= init));
+        self.model.add(Dense(512, activation="relu"));
+        self.model.add(Dense(256,activation="relu")); 
+        self.model.add(Dense(128,activation="relu"));
+        self.model.add(Dense(64,activation="relu"));
+        self.model.add(Dense(32,activation="relu"));
+        self.model.add(Dense(16,activation="relu"));
+        self.model.add(Dense(1,activation="sigmoid"));
+        # learning rate 0.001
+        optimizer_adam = optimizers.Adam(lr=lr,decay = decay)
+        self.model.compile(loss='binary_crossentropy', optimizer= optimizer_adam,  metrics = ["accuracy"])         
+        return self.model
+
+
+
+def main():
+    label_data = PreprocessData();
+    X, Y = label_data.load_label_data('/Users/shanyang/Downloads/Face_of_Idou/imagesout')
+    # save data as check point 
+    label_data.save('/Users/shanyang/Downloads/Face_of_Idou/imagesout_XY.npz')
+    
+    ## feature embemdding 
+    FE = FeatureEmbemding(X,Y);
+    FE.generate_features();
+    FE.feature_rescale();
+    # save as check point 
+    FE.save('/Users/shanyang/Downloads/Face_of_Idou/imagesout_feature_label.npz');
+    
+    features, labels = FE.get_feature_label();
+    x_train,x_test,y_train,y_test = train_test_split(features, labels, test_size = 0.2, random_state = 42)
+   
+    ## training 
+    model = MyModel();
+    model.build_model();
+    hist = model.model.fit(x_train,y_train,validation_split=0.1,shuffle=True,epochs = 300)
+    model.model.predict_class(x_test,y_test)
+    
+    
+    
+    
+    
+
+        
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
---
+++ image_preprocess.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 00:38:04 2020
+
+@author: shanyang
+"""
+from PIL import Image
+import glob
+import face_recognition
+import cv2
+import numpy as np
+import sys
+import os
+import time
+import logging
+#import multiprocessing as mp
+
+
+class image_preprocess:
+    def find_align_crop_image(self, input_path, output_path):
+      image = face_recognition.load_image_file(input_path,)      
+      self.face_locations = face_recognition.face_locations(image, model="cnn")
+      self.face_landmarks = face_recognition.face_landmarks(image)
+      
+      if(len(self.face_locations) >1):
+          print('More than one face in this image so ignore the image.')
+          return
+      else:
+          try:
+              self.output=self._alignment(image, self.face_landmarks)
+              pil_image = Image.fromarray(self.output)
+              pil_image.save(output_path)
+          except:
+              print('Unexprected error:',sys.exc_info()[0]);
+       
+    def _alignment(self, image, face_landmarks):
+        leftEyePts = face_landmarks[0]['left_eye']
+        rightEyePts = face_landmarks[0]['right_eye']
+        leftEyeCenter = np.array(leftEyePts).mean(axis=0).astype("int")
+        rightEyeCenter = np.array(rightEyePts).mean(axis=0).astype("int")
+
+        # calculat the rotation angle
+        dY = rightEyeCenter[1] - leftEyeCenter[1]
+        dX = rightEyeCenter[0] - leftEyeCenter[0]
+        angle = np.degrees(np.arctan2(dY, dX))
+        
+        # set the ideal left and right eye location
+        desiredLeftEye=(0.35, 0.45)
+        desiredRightEyeX = 1.0 - desiredLeftEye[0]
+        
+        # set the croped image(face) size after rotaion.
+        desiredFaceWidth = 224
+        desiredFaceHeight = 224
+        
+        # calculate the desired the scale after crop 
+        dist = np.sqrt((dX ** 2) + (dY ** 2))
+        desiredDist = (desiredRightEyeX - desiredLeftEye[0])
+        desiredDist *= desiredFaceWidth
+        scale = desiredDist / dist
+        
+        # calculate the center of eyes
+        eyesCenter = ((leftEyeCenter[0] + rightEyeCenter[0]) //2.0,(leftEyeCenter[1] + rightEyeCenter[1]) // 2.0)
+        
+        # calculat the rotation matrix
+        M = cv2.getRotationMatrix2D(eyesCenter, angle, scale)
+        tX = desiredFaceWidth * 0.5
+        tY = desiredFaceHeight * desiredLeftEye[1]
+        M[0, 2] += (tX - eyesCenter[0])
+        M[1, 2] += (tY - eyesCenter[1])
+        (w, h) = (desiredFaceWidth, desiredFaceHeight)
+        output = cv2.warpAffine(image, M, (w, h),flags=cv2.INTER_CUBIC)
+        return output
+    
+    def input_and_output(self,input_dir,output_dir):
+        start_time = time.time()
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+            
+        #mp.log_to_stderr
+        #logger =mp.get_logger()
+        #logger.setLevel(logging.INFO)       
+        #pool=mp.Pool(os.cpu_count())  
+        
+        for image_dir in os.listdir(input_dir):
+            image_output_dir = os.path.join(output_dir, os.path.basename(os.path.basename(image_dir)))
+            if not os.path.exists(image_output_dir):
+                os.makedirs(image_output_dir)
+    
+            image_paths = glob.glob(os.path.join(input_dir,image_dir,'*.jpg'))
+            
+            for index, image_file in enumerate(image_paths):
+                image_output_dir = os.path.join(output_dir, os.path.basename(os.path.dirname(image_file)))                
+                output_file = os.path.join(image_output_dir,str(index)+'.jpg')    
+                self.find_align_crop_image(image_file, output_file)          
+                #pool.apply_async(self.find_align_crop_image,(image_file, output_file))
+                
+        #pool.close()
+        #pool.join()
+        end_time = time.time()
+        total_time = end_time-start_time;
+        logging.info('Completed in {} seconds'.format(total_time))
+        return
+            
+if __name__=='__main__':
+    test = image_preprocess()
+    test.input_and_output('./images/','./imagesout/')
+
+
+
